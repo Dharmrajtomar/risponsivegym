dd24ca186bd32e5a4a086c519e3897bc8f0e5cc8
Dharmrajtomar <dharmrajtomar43@gmail.com>
2025-01-23 20:58:37 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Navebaar from "../src/components/gymwebpage/htmlComponent/Navebaar.js";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import {HashRouter, Route, Router, Routes } from "react-router-dom";
 import Loginform from "./components/gymwebpage/htmlComponent/Loginform.js";
 import SellingItem from "./components/gymwebpage/htmlComponent/SellingItem.js";
 const App = () => {
   return (
     <>
-    
-      <BrowserRouter basename="/risponsive-gym-front-web">
+      <HashRouter>
+      {/* <BrowserRouter> */}
         <Routes>
           <Route index element={<Navebaar />} />
           <Route path="join" element={<Loginform />} />
           <Route path="sell" element={<SellingItem />} />
         </Routes>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
+      </HashRouter>
     </>
   );
 };
